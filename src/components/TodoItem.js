90a7141e7555b6974fa049e7215dd83dfad09bac
ediprako <ediprakoso@gmail.com>
2020-02-23 16:56:47 +0700

    initial commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class TodoItem extends Component {
+    getStyle=()=>{
+        return {
+            background: '#f4f4f4',
+            padding:'10px',
+            borderBottom:'1px #ccc dotted',
+            textDecoration:this.props.todo.completed ? 'line-through' : 'none'
+        }
+    }
+
+    getChecked=()=>{
+        return this.props.todo.completed ? "checked" : "";
+    }
+
+    render() {
+        const { id,title} = this.props.todo
+        return (
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this,id)} checked={this.getChecked()} /> {' '}
+                    {title}
+                    <button style={btnStyle} onClick={this.props.delTodo.bind(this,id)}>x</button>
+                </p>
+            </div>
+        );
+    }
+}
+
+const btnStyle = {
+    background:'#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding:'5px 8px',
+    borderRadius:'50%',
+    cursor:'pointer',
+    float:'right'
+}
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired,
+    markComplete:PropTypes.func.isRequired,
+    delTodo:PropTypes.func.isRequired
+};
+
+export default TodoItem;
